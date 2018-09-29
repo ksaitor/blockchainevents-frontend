5a021974bbc7d9eb9f0bd31b4dad11e3feb1e9d3
Raman Shalupau <raman.shalupau@gmail.com>
2018-09-29 23:02:52 +0800

    converting footer to pure function

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,21 +4,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Container, List, Icon } from 'semantic-ui-react'
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <div className='Footer'>
-        <Container text textAlign='center'>
-          <List horizontal divided link>
-            <List.Item>Blockchain Event &copy; {(new Date()).getFullYear()}</List.Item>
-            <List.Item as={Link} to='https://cryptojobslist.typeform.com/to/klhneI' target='_blank'>
-              Submit an Event
-            </List.Item>
-          </List>
-        </Container>
-      </div>
-    );
-  }
-}
-
-export default Footer;
+export default Footer = () => (
+  <div className='Footer'>
+    <Container text textAlign='center'>
+      <List horizontal divided link>
+        <List.Item>BlockchainEvent.co &copy; {(new Date()).getFullYear()}</List.Item>
+        <List.Item as={Link} to='/submit'>
+          Submit an Event
+        </List.Item>
+      </List>
+    </Container>
+  </div>
+)
